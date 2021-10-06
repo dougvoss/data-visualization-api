844ccc6baf67c13a5f8b4a4a79bae93484ef5880
Douglas Voss <douglas.voss88@gmail.com>
2021-10-06 11:18:45 -0300

    Update dbconnection.js

--- src/config/dbconnection.js
+++ src/config/dbconnection.js
@@ -2,11 +2,11 @@ var mysql = require('mysql');
 
 var connection = mysql.createConnection({
 
-    host: 'rdsramais.cncn4cn3auep.sa-east-1.rds.amazonaws.com',
-    port: '3306',
-    user: 'sispack',
-    password: 'masterkey',
-    database: 'graficos',
+    host: '',
+    port: '',
+    user: '',
+    password: '',
+    database: '',
 
 });
 module.exports = connection;
